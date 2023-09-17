55260aab24821d38a4cb47df776f3aa72b5cc6d4
Ayusman <ayusmanmohanty123@gmail.com>
2023-09-17 19:21:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+    public static String question[] ={
+        "Which company owns the android?",
+        "Which one is not the programming language?",
+        "Which of the following is not a layout type in Android?",
+        "Which company owns the Apple?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {" Linear Layout","Grid Layout","Absolute Layout","Table Layout"},
+            {"Samsung","Apple","Nokia","Google"}
+
+    };
+
+    public static String correctAnswers[] ={
+            "Google",
+            "Notepad",
+            "Absolute Layout",
+            "Apple"
+
+    };
+}
